b3910a0f79bae7ea001cbce8ac101d2913acf64c
slim <slimabida21@gmail.com>
2024-12-16 21:11:02 +0100

    config menu-burger app.js OK

---
+++ app.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector('.nav-burger'); 
+const nav = document.querySelector('.nav-menu'); 
+
+const allLiens = document.querySelectorAll('.nav-menu li a');
+
+burger.addEventListener('click',() =>{
+    burger.classList.toggle('active'); 
+    nav.classList.toggle('active'); 
+
+})
+
+allLiens.forEach(item=>{
+    item.addEventListener('click',()=>{
+        burger.classList.remove('active'); 
+    nav.classList.remove('active'); 
+    })
+})
